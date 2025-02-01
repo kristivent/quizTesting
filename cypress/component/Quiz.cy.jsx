82d4cb5d2ad8dbf6d35ff750d851204789fef3ff
kristivent <ventk84@gmail.com>
2025-02-01 15:10:01 -0500

    updated questions and test files in cypress

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,1 +1,18 @@
 //component tests for quiz component
+
+import React from 'react';
+import { mount } from 'cypress/react';
+import Quiz from '../../client/src/components/Quiz';
+
+describe('Quiz Component', () => {
+  it('should render the Quiz component', () => {
+    mount(<Quiz />);
+    cy.get('[data-cy=start-button]').should('be.visible');
+  });
+
+  it('should start the quiz when the start button is clicked', () => {
+    mount(<Quiz />);
+    cy.get('[data-cy=start-button]').click();
+    cy.get('[data-cy=question]').should('be.visible');
+  });
+});
--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,1 +1,36 @@
 // End-to-end tests for Tech Quiz
+
+describe('Tech Quiz', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+  
+    it('should start the quiz when the start button is clicked', () => {
+      cy.get('[data-cy=start-button]').click();
+      cy.get('[data-cy=question]').should('be.visible');
+    });
+  
+    it('should display the next question when an answer is selected', () => {
+      cy.get('[data-cy=start-button]').click();
+      cy.get('[data-cy=answer-button]').first().click();
+      cy.get('[data-cy=question]').should('be.visible');
+    });
+  
+    it('should display the final score when the quiz is completed', () => {
+      cy.get('[data-cy=start-button]').click();
+      for (let i = 0; i < 10; i++) {
+        cy.get('[data-cy=answer-button]').first().click();
+      }
+      cy.get('[data-cy=score]').should('be.visible');
+    });
+  
+    it('should allow starting a new quiz after completion', () => {
+      cy.get('[data-cy=start-button]').click();
+      for (let i = 0; i < 10; i++) {
+        cy.get('[data-cy=answer-button]').first().click();
+      }
+      cy.get('[data-cy=score]').should('be.visible');
+      cy.get('[data-cy=start-new-quiz-button]').click();
+      cy.get('[data-cy=start-button]').should('be.visible');
+    });
+  });
